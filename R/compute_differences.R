024ebe9a18d6aae2fcfb031f57c2f01ae5e04b1b
ArthurLeroy <arthur.leroy.pro@gmail.com>
2023-07-18 01:17:42 +0200

    Add multivariate version for identify_diff

--- R/compute_differences.R
+++ R/compute_differences.R
@@ -12,6 +12,10 @@
 #'     peptide.
 #' @param CI_level A number, defining the order of quantile chosen to assess
 #'     differences between groups.
+#' @param nb_samples A number (optional), indicating the
+#'     number of samples to draw from the posteriors for computing mean and
+#'     credible intervals . Only used if \code{posterior} is multivariate,
+#'     typically coming from a \code{multi_posterior_mean()} function.
 #'
 #' @return A tibble, indicating which peptides and groups seem to be different
 #' @export
@@ -20,42 +24,54 @@
 #' TRUE
 identify_diff <- function(
     posterior,
-    CI_level = 0.05){
+    CI_level = 0.05,
+    nb_samples = 1000){
 
   ## We could think of defining a multivariate criterion for differences
 
-  db_diff <- posterior %>%
-    dplyr::mutate('sigma' = sqrt(.data$beta / (.data$lambda * .data$alpha)),
-                  'df' =  2 * .data$alpha) %>%
-    dplyr::reframe(
-      .data$Peptide, .data$Group, .data$mu,
-      'CI_inf' = extraDistr::qlst(
-        p = CI_level/2,
-        df =.data$df,
-        mu = .data$mu,
-        sigma = .data$sigma,
-        lower.tail = T),
-      'CI_sup' =  extraDistr::qlst(
-        p = CI_level/2,
-        df =.data$df,
-        mu = .data$mu,
-        sigma = .data$sigma,
-        lower.tail = F)
-    )
+  if('Sigma' %in% names(posterior)){
+    db_diff <- posterior %>%
+      sample_distrib(10) %>%
+      dplyr::group_by(.data$Peptide, .data$Group) %>%
+      dplyr::reframe('mu' = mean(.data$Sample),
+                     'CI_inf' = stats::quantile(.data$Sample, CI_level/2),
+                     'CI_sup' = stats::quantile(.data$Sample, 1- CI_level/2))
 
-  db_diff %>%
-    tidyr::expand_grid('Group2' = unique(.data$Group)) %>%
-    dplyr::left_join(
-      db_diff %>%
-        dplyr::rename('Group2' = .data$Group,
-                      'mu2' = .data$mu,
-                      'CI_inf2' = .data$CI_inf,
-                      'CI_sup2' = .data$CI_sup),
-      by = c("Peptide", 'Group2')) %>%
-    dplyr::filter(.data$Group != .data$Group2) %>%
-    dplyr::mutate( 'Distinct' = (.data$CI_sup < .data$CI_inf2) |
-                     (.data$CI_inf > .data$CI_sup2)
-    ) %>%
-    return()
+  } else{
+    db_diff <- posterior %>%
+      dplyr::mutate('sigma' = sqrt(.data$beta / (.data$lambda * .data$alpha)),
+                    'df' =  2 * .data$alpha) %>%
+      dplyr::reframe(
+        .data$Peptide, .data$Group, .data$mu,
+        'CI_inf' = extraDistr::qlst(
+          p = CI_level/2,
+          df =.data$df,
+          mu = .data$mu,
+          sigma = .data$sigma,
+          lower.tail = T),
+        'CI_sup' =  extraDistr::qlst(
+          p = CI_level/2,
+          df =.data$df,
+          mu = .data$mu,
+          sigma = .data$sigma,
+          lower.tail = F)
+      )
+  }
+
+    db_diff %>%
+      tidyr::expand_grid('Group2' = unique(.data$Group)) %>%
+      dplyr::left_join(
+        db_diff %>%
+          dplyr::rename('Group2' = .data$Group,
+                        'mu2' = .data$mu,
+                        'CI_inf2' = .data$CI_inf,
+                        'CI_sup2' = .data$CI_sup),
+        by = c("Peptide", 'Group2')) %>%
+      dplyr::filter(.data$Group != .data$Group2) %>%
+      dplyr::mutate( 'Distinct' = (.data$CI_sup < .data$CI_inf2) |
+                       (.data$CI_inf > .data$CI_sup2)
+      ) %>%
+      return()
+
 }
 
